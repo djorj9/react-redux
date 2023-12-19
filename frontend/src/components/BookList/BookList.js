0091f3b1775020d74450f760643391c9709523d1
doomik <djorj9@gmail.com>
2023-12-19 13:59:23 +0200

    Добавив redux для добавлення книг

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,10 +1,25 @@
-import './BookList.css'
+import { useSelector } from "react-redux";
+import "./BookList.css";
 const BookList = () => {
-    return (
-      <div className="app-block book-list">
-        <h2>Список книг</h2>
-      </div>
-    );
-  };
-  
-  export default BookList;
+  const books = useSelector((state) => state.books);
+  return (
+    <div className="app-block book-list">
+      <h2>Список книг</h2>
+      {books.lenght === 0 ? (
+        "Немає доступних книг"
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={i}>
+              <div className="book-info">
+                {++i}. {book.title} від <strong> {book.author}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default BookList;
