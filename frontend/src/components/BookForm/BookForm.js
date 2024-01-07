ebbd283e128f7bc35740335d05bfe09b0e52c097
doomik <djorj9@gmail.com>
2024-01-07 21:27:45 +0200

    Добавив аксіос і вивід книг з апі

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import  createBookWithId  from "../../utils/createBookWithId"
+import createBookWithId from "../../utils/createBookWithId";
 import { addBook } from "../../redux/slices/booksSlice";
-import  booksData from "../../data/books.json"
+import booksData from "../../data/books.json";
 import "./BookForm.css";
 const BookForm = () => {
   const [title, setTitle] = useState("");
@@ -10,21 +11,31 @@ const BookForm = () => {
   const dispatch = useDispatch();
 
   const handleAddRandomBook = () => {
-     const randomIndex = Math.floor(Math.random() * booksData.length)
-     const randomBook = booksData[randomIndex]
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
 
-  
-     dispatch(addBook(createBookWithId(randomBook)));
-  }
+    dispatch(addBook(createBookWithId(randomBook)));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
-      dispatch(addBook(createBookWithId({title,author})));
+      dispatch(addBook(createBookWithId({ title, author })));
       setTitle("");
       setAuthor("");
     }
   };
+  const handleAddRandomBookViaAPI = async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/random-book");
+      if (res?.data?.title && res?.data?.author) {
+        dispatch(addBook(createBookWithId(res.data)));
+      }
+    } catch (error) {
+      console.log("Помилка запиту axios", error);
+    }
+  };
+
   return (
     <div className="app-block book-form">
       <h2>Додати нову книгу</h2>
@@ -48,7 +59,12 @@ const BookForm = () => {
           />
         </div>
         <button type="submit">Добавити книгу</button>
-        <button type="button" onClick={handleAddRandomBook}>Добавити випадкову книгу</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Добавити випадкову книгу
+        </button>
+        <button type="button" onClick={handleAddRandomBookViaAPI}>
+          Добавити випадкову книгу з Апі
+        </button>
       </form>
     </div>
   );
